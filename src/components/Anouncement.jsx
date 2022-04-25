2c7eb176a65d917c483dd151711bf68097534952
Emmanuel Hateka <emmanuelhateka.com>
2022-04-25 15:21:24 +0000

    finished with the anouncement and started the slider

--- src/components/Anouncement.jsx
+++ src/components/Anouncement.jsx
@@ -1,5 +1,17 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 30px;
+  background-color: teal;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
+`;
 const Anouncement = () => {
-  return <div>Anouncement</div>;
+  return <Container>Super Deal! Free shipping on orders over $50</Container>;
 };
 
 export default Anouncement;
