ca136b8f29f16fad7dfda7676629c0acb664ef88
Emmanuel Hateka <emmanuelhateka.com>
2022-04-28 21:27:38 +0000

    cart page in progress

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -18,16 +18,26 @@ const Top = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  padding: 20px;
 `;
 
 const TopButton = styled.button`
   padding: 10px;
   font-weight: 600;
   cursor: pointer;
+  border: ${(props) => props.type === "filled" && "none"};
+  background-color: ${(props) =>
+    props.type === "filled" ? "black" : "transparent"};
+  color: ${(props) => props.type === "filled" && "white"};
 `;
 
 const TopTexts = styled.div``;
-const TopText = styled.span``;
+
+const TopText = styled.span`
+  text-decoration: underline;
+  cursor: pointer;
+  margin: 0px 10px;
+`;
 
 const Bottom = styled.div``;
 
@@ -44,7 +54,7 @@ const Cart = () => {
             <TopText>Shopping Bag(2)</TopText>
             <TopText>Your Wishlist(0)</TopText>
           </TopTexts>
-          <TopButton>CHECKOUT NOW</TopButton>
+          <TopButton type="filled">CHECKOUT NOW</TopButton>
         </Top>
         <Bottom></Bottom>
       </Wrapper>
